02c8165ba42fbe7ab2c2b74a1bc822e0ed39ecf1
shrutisinghal <shrutis@thoughtworks.com>
2020-08-16 16:14:26 +0530

    call cs functions from JS

--- Client/wwwroot/alert.js
+++ Client/wwwroot/alert.js
@@ -1,3 +1,14 @@
 function Alert(message) {
            alert(message);
 }
+
+function setElementById(id, text){
+    document.getElementById(id).innerText = text;
+}
+
+function getPassCode(maxValue){
+    DotNet.invokeMethodAsync('Client', 'GeneratePassCode', maxValue)
+          .then(result => {
+              setElementById("passCodeSpan", result)
+          });
+}
